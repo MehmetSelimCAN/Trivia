b21b2617c2915e933e3002e61f7f64542a642a7d
Mehmet Selim CAN <MehmetSelimCN@hotmail.com>
2023-08-25 15:49:24 +0300

    Fix score manager's zenject binding

--- Trivia/Assets/Scripts/GameInstaller.cs
+++ Trivia/Assets/Scripts/GameInstaller.cs
@@ -8,5 +8,6 @@ public class GameInstaller : MonoInstaller
     public override void InstallBindings()
     {
         Container.Bind<CanvasManager>().AsSingle().WithArguments(canvasManagerMenus);
+        Container.Bind<ScoreManager>().AsSingle().NonLazy();
     }
 }
