5f70d177f9981ce85a855c613d8c356e7856fd8f
Mehmet Selim CAN <MehmetSelimCN@hotmail.com>
2023-08-25 15:28:10 +0300

    Add timer to gameplay menu

---
+++ Trivia/Assets/Scripts/TimerUI.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TimerUI : MonoBehaviour
+{
+    private float maxTimer = 5f;
+    private float timer;
+    [SerializeField] private Image timerFill;
+    [SerializeField] private Image timerOutline;
+    [SerializeField] private Transform timeOutPopup;
+
+    private bool isGamePlaying;
+
+    private void Awake()
+    {
+        timer = maxTimer;
+    }
+
+    private void Update()
+    {
+        if (isGamePlaying)
+        {
+            timer -= Time.deltaTime;
+            timerFill.fillAmount = timer / maxTimer;
+
+            if (timer <= 0)
+            {
+                TimeOut();
+            }
+        }
+    }
+
+    private void TimeOut()
+    {
+        timerFill.gameObject.SetActive(false);
+        timerOutline.gameObject.SetActive(false);
+        timeOutPopup.transform.DOScale(Vector3.one, 0.25f);
+        isGamePlaying = false;
+    }
+
+    public void ResetTimer()
+    {
+        timer = maxTimer;
+        timerFill.gameObject.SetActive(true);
+        timerOutline.gameObject.SetActive(true);
+        timeOutPopup.transform.localScale = Vector3.zero;
+    }
+
+    private void OnEnable()
+    {
+        isGamePlaying = true;
+    }
+
+    private void OnDisable()
+    {
+        isGamePlaying = false;
+    }
+}
