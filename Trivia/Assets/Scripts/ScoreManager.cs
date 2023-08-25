75c73cb611d550f0b333336a6c0b8d28ac61e1ee
Mehmet Selim CAN <MehmetSelimCN@hotmail.com>
2023-08-25 15:51:04 +0300

    Add link between score manager and event manager

--- Trivia/Assets/Scripts/ScoreManager.cs
+++ Trivia/Assets/Scripts/ScoreManager.cs
@@ -1,7 +1,7 @@
 using System;
 using Zenject;
 
-public class ScoreManager
+public class ScoreManager : IInitializable, IDisposable
 {
     private int totalScore = 0;
     public int TotalScore { get { return totalScore; } }
@@ -10,18 +10,35 @@ public class ScoreManager
     private int wrongAnswerPenalty = 5;
     private int timeoutPenalty = 3;
 
+    public void Initialize()
+    {
+        EventManager.GainScoreEvent += GainScore;
+        EventManager.LoseScoreEvent += LoseScore;
+        EventManager.TimeOutEvent += TimeOut;
+    }
+
+    public void Dispose()
+    {
+        EventManager.GainScoreEvent -= GainScore;
+        EventManager.LoseScoreEvent -= LoseScore;
+        EventManager.TimeOutEvent -= TimeOut;
+    }
+
     private void GainScore()
     {
         totalScore += correctAnswerPoint;
+        EventManager.ScoreChanged();
     }
 
     private void LoseScore()
     {
         totalScore -= wrongAnswerPenalty;
+        EventManager.ScoreChanged();
     }
 
     private void TimeOut()
     {
         totalScore -= timeoutPenalty;
+        EventManager.ScoreChanged();
     }
 }
